820284e9a554402f0e47f07bf7b2dea12a91d287
Ana Silva <anasilva@lsd.ufcg.edu.br>
2019-06-05 16:57:14 -0300

    update

--- puppeteer-scrapper/index.js
+++ puppeteer-scrapper/index.js
@@ -5,37 +5,40 @@ var fs = require("fs");
 // MY OCD of colorful console.logs for debugging... IT HELPS
 const error = chalk.bold.red;
 const success = chalk.keyword("green");
+const lastPageNumber = 10;
+
+loveMondaysLogin = () =>
+{
+
+}
 
 (async () => {
   try {
     // open the headless browser
-    var browser = await puppeteer.launch({ headless: false });
+    var browser = await puppeteer.launch({executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe', headless: false });
     // open a new page
     var page = await browser.newPage();
     // enter url in page
-    await page.goto(`https://www.lovemondays.com.br/trabalhar-na-accenture/avaliacoes`);
-    await page.waitForSelector("ul.lm-List-default");
-
-    var news = await page.evaluate(() => {
-      var username = document.querySelectorAll(`span.reviewer`);
-      var avaliation = document.querySelectorAll(`div.lm-Review-contribution`);
-      var titleLinkArray = [];
-      for (var i = 0; i < username.length; i++) {
-        titleLinkArray[i] = {
-          user: titleNodeList[i].innerText.trim(),
-          avaliation: avaliation[i].innerText.trim()
-        };
-      }
-      return titleLinkArray;
-    });
-    // console.log(news);
-    await browser.close();
-    // Writing the news inside a json file
-    fs.writeFile("teste.json", JSON.stringify(news), function(err) {
-      if (err) throw err;
-      console.log("Saved!");
-    });
-    console.log(success("Browser Closed"));
+    for (let index = 1; index < lastPageNumber; index++) {      
+      await page.goto(`URLHERE` +index);
+      await page.waitForSelector("ul.lm-List-default");
+      var ratings = await page.evaluate(() => {
+        var job = document.querySelectorAll(`span.reviewer`);
+        var avaliation = document.querySelectorAll(`div.lm-Review-contribution`);
+        var titleLinkArray = [];
+        for (var i = 0; i < job.length; i++) {
+            titleLinkArray[i] = {
+              user: job[i].innerText,
+              avaliation: avaliation[i].innerText
+            };
+        }
+        return titleLinkArray;
+      });
+      fs.writeFile(index + ".json", JSON.stringify(ratings), function(err) {
+        if (err) throw err;
+        console.log(index);
+      });
+    }
   } catch (err) {
     // Catch and display errors
     console.log(error(err));
