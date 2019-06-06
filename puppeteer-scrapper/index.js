47ce2f9efe506113251faf2f1b3042d616f9721c
Ana Silva <anasilva@lsd.ufcg.edu.br>
2019-06-06 14:01:25 -0300

    up

--- puppeteer-scrapper/index.js
+++ puppeteer-scrapper/index.js
@@ -15,30 +15,35 @@ loveMondaysLogin = () =>
 (async () => {
   try {
     // open the headless browser
-    var browser = await puppeteer.launch({executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe', headless: false });
+    var browser = await puppeteer.launch({headless: false });
     // open a new page
     var page = await browser.newPage();
+
+    await page.goto(`https://www.lovemondays.com.br/usuarios/entrar#email-sign-in`, {waitUntil: 'networkidle2'});
+
+    //document.getElementById("email-sign-in").setAttribute("aria-expanded", "true");
+    // await page.type('input[name=search]', 'Adenosine triphosphate');
     // enter url in page
-    for (let index = 1; index < lastPageNumber; index++) {      
-      await page.goto(`URLHERE` +index);
-      await page.waitForSelector("ul.lm-List-default");
-      var ratings = await page.evaluate(() => {
-        var job = document.querySelectorAll(`span.reviewer`);
-        var avaliation = document.querySelectorAll(`div.lm-Review-contribution`);
-        var titleLinkArray = [];
-        for (var i = 0; i < job.length; i++) {
-            titleLinkArray[i] = {
-              user: job[i].innerText,
-              avaliation: avaliation[i].innerText
-            };
-        }
-        return titleLinkArray;
-      });
-      fs.writeFile(index + ".json", JSON.stringify(ratings), function(err) {
-        if (err) throw err;
-        console.log(index);
-      });
-    }
+    // for (let index = 1; index < lastPageNumber; index++) {      
+    //   await page.goto(`URLHERE` +index);
+    //   await page.waitForSelector("ul.lm-List-default");
+    //   var ratings = await page.evaluate(() => {
+    //     var job = document.querySelectorAll(`span.reviewer`);
+    //     var avaliation = document.querySelectorAll(`div.lm-Review-contribution`);
+    //     var titleLinkArray = [];
+    //     for (var i = 0; i < job.length; i++) {
+    //         titleLinkArray[i] = {
+    //           user: job[i].innerText,
+    //           avaliation: avaliation[i].innerText
+    //         };
+    //     }
+    //     return titleLinkArray;
+    //   });
+    //   fs.writeFile(index + ".json", JSON.stringify(ratings), function(err) {
+    //     if (err) throw err;
+    //     console.log(index);
+    //   });
+    //}
   } catch (err) {
     // Catch and display errors
     console.log(error(err));
