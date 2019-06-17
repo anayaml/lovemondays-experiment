cb965892a537979d8f095d7c592911fc25c4be78
Ana Silva <anasilva@lsd.ufcg.edu.br>
2019-06-17 15:19:12 -0300

    up

--- generate-dataset-spreadsheet/app.py
+++ generate-dataset-spreadsheet/app.py
@@ -5,12 +5,12 @@ import ast
 
 reviews_data = open('reviews.csv', 'w+', encoding="utf-8")
 
-csv_writer = csv.writer(reviews_data, delimiter=';', lineterminator='\n')
+csv_writer = csv.writer(reviews_data, delimiter=',', lineterminator='\n')
 
 empresas = ['accenture', 'stefanini', 'ibm', 'totvs', 'algar-tech', 'sonda-it', 'hp-inc', 'dell', 'linx', 'oracle', 'resource-it-solutions', 'ericsson', 'vivo-telefonica-brasil', 'tivit', 'tim', 'claro-brasil', 'b2w-digital', 'concentrix', 'nextel-telecomunicacoes']
 
 count = 1
-csv_writer.writerow(["user", "avaliation"])
+csv_writer.writerow(["user", "avaliation", "former_employeer"])
 for root, dirs, files in os.walk('C:\\Users\\asana\\Workspace\\lovemondays-experiment\\generate-dataset-spreadsheet//'):
     for name in files:
         if name.endswith((".json")):
@@ -18,7 +18,11 @@ for root, dirs, files in os.walk('C:\\Users\\asana\\Workspace\\lovemondays-exper
                 filename = "data\\" + i + "\\" + name
                 actual_json_file = open(filename, 'r', encoding='utf-8')
                 currentFile = json.load(actual_json_file)
-                csv_writer.writerow([currentFile[0]["user"], currentFile[1]["avaliation"]])
+                if  "Ex-funcionário" in currentFile[0]["user"]:
+                    csv_writer.writerow([currentFile[0]["user"], currentFile[1]["avaliation"], True])
+                else:
+                    csv_writer.writerow([currentFile[0]["user"], currentFile[1]["avaliation"], False])
                 print("Processing file #" + str(count))
                 count += 1
+print("Done")
 reviews_data.close()
