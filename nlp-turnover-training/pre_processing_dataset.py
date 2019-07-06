0a0dd0779ada6966dd5aa15093ec7063f241f8c5
Ana Silva <anasilva@lsd.ufcg.edu.br>
2019-07-06 20:33:24 -0300

    implementing aspect based sentiment analysis

---
+++ nlp-turnover-training/pre_processing_dataset.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import csv
+from google.cloud import translate
+
+def translate_training_dataset_to_english():
+    en_training_csv = open('/data/en_training_dataset.csv', 'w+', encoding="utf-8")
+    csv_writer = csv.writer(en_training_csv, delimiter=',', lineterminator='\n')
+    translator = translate.Client()
+    portguese_dataset = pd.read_csv('/data/pt_training_dataset.csv', encoding='utf8')
+    csv_writer.writerow(["review", "aspect", "aspect_category", "sentiment"])
+    for reg in portguese_dataset.itertuples():
+        pro = translator.translate(reg.pros, target_language='en')
+        cons = translator.translate(reg.cons, target_language='en')
+        csv_writer.writerow([pro['translatedText'], "NA", "NA", "positive"])
+        csv_writer.writerow([cons['translatedText'], "NA", "NA", "negative"])
+
+def translate_final_dataset_to_english():
+    en_dataset_csv = open("/data/en_experiment_dataset.csv", 'w+', encoding="utf8")
+    data_writer = csv.writer(en_dataset_csv, delimiter=',', lineterminator="\n")
+    count = 0
+    translator = translate.Client()
+    pt_dataset = pd.read_csv('/data/dataset_without_pilot_data.csv', encoding='utf8')
+    data_writer.writerow(["review", "former_emp"])
+    for reg in pt_dataset.itertuples():
+        pro = translator.translate(reg.pros, target_language='en')
+        cons = translator.translate(reg.cons, target_language='en')
+        data_writer.writerow([pro['translatedText'], reg.former_employee])
+        data_writer.writerow([cons['translatedText'], reg.former_employee])
+        count +=1
+        print(count)
+
+print("--- Training Dataset ----")
+translate_training_dataset_to_english()
+print("--- Final Dataset ----")
+translate_final_dataset_to_english()
---
+++ nlp-turnover-training/processing_data.py
@@ -0,0 +1,25 @@
+# NLTK
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+nltk.download('stopwords')
+
+#Spacy
+import spacy
+nlp = spacy.load('en')
+
+# Other
+import re
+import json
+import string
+import numpy as np
+import pandas as pd
+
+import warnings
+warnings.filterwarnings('ignore')
+
+#Keras
+from keras.models import load_model
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+
